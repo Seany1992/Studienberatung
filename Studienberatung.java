aeac5afdd8008596102d235c109226f67691e6ea
Seany1992 <s.gocks@live.de>
2017-10-25 20:42:39 +0200

    Main und Scanner hinzugefügt

---
+++ Studienberatung.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Studienberatung {
+	public static void main (String[] args) {
+		
+		 boolean ready = true;
+		
+		
+			System.out.println("Hast du Abitur?");
+	
+			System.out.println("n oder j:");
+			Scanner scanner = new Scanner(System.in);
+			
+		
+		
+		
+		
+	}
+	
+	
+}
