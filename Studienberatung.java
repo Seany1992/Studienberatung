2d03f9b077b626368ad953f01b8d3e2fa4650bc4
Seany1992 <s.gocks@live.de>
2017-10-25 20:49:27 +0200

    Antwort vom Scanner geaddet

--- Studienberatung.java
+++ Studienberatung.java
@@ -2,14 +2,14 @@ import java.util.*;
 
 public class Studienberatung {
 	public static void main (String[] args) {
-		
-		 boolean ready = true;
-		
+		Scanner scanner = new Scanner(System.in);
+		boolean ready = true;
+		 
 		
 			System.out.println("Hast du Abitur?");
 	
 			System.out.println("n oder j:");
-			Scanner scanner = new Scanner(System.in);
+			String input = scanner.nextLine();
 			
 		
 		
